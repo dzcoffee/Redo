b4996bf81e28518d0bd8992c3fbb973eb0159cac
Urchinode <vrdhan212@gmail.com>
2024-04-21 22:24:43 +0900

    [client] config: eslint rule 추가

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,5 +11,53 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+    'no-restricted-syntax': 'off',
+    'prettier/prettier': 'off',
+    'import/no-cycle': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '_'
+      }
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+        allowDirectConstAssertionInArrowFunctions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: false
+      }
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 160,
+        ignoreStrings: true,
+        ignorePattern: 'class="[^"]*"'
+      }
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true
+      }
+    ],
+    'vue/first-attribute-linebreak': [
+      'error',
+      {
+        singleline: 'ignore',
+        multiline: 'beside'
+      }
+    ]
   }
 }
