f312cba53a791a0b58d4a023950b17402aed7ea0
vrdhan212 <vrdhan212@gmail.com>
2024-04-22 12:58:03 +0900

    [client] config: vuetify 플러그인 추가

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,12 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import vuetify from '@/plugins/vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
