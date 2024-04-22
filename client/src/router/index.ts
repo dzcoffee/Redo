0d3e9ff6ad99b391e7b9d2fda765424e42c3e558
vrdhan212 <vrdhan212@gmail.com>
2024-04-22 12:58:24 +0900

    [client] feat: url 경로

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+
+const subRoutes: RouteRecordRaw[] = [
+  { path: '/memo', name: 'Memo', component: HomeView },
+  { path: 'memo/create', name: 'MemoCreate', component: HomeView },
+  { path: 'memo/edit', name: 'MemoEdit', component: HomeView },
+  { path: 'memo/:id', name: 'MemoView', component: HomeView },
+  { path: '/quiz', name: 'Quiz', component: HomeView },
+  { path: 'quiz/create', name: 'QuizCreate', component: HomeView },
+  { path: 'quiz/game', name: 'QuizGame', component: HomeView },
+  { path: 'quiz/archive', name: 'QuizArchive', component: HomeView }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +18,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      children: subRoutes
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: HomeView // Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: HomeView // SignUp
     },
     {
       path: '/about',
@@ -16,6 +38,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      name: 'NotFound',
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
     }
   ]
 })
