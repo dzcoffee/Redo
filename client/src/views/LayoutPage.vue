0924c17540bf4a617b89a63980267a856a391ce4
vrdhan212 <vrdhan212@gmail.com>
2024-04-30 13:24:48 +0900

    [client] feat: 퀴즈 풀기 페이지 UI

--- client/src/views/LayoutPage.vue
+++ client/src/views/LayoutPage.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row class="h-100 pa-0 ma-0" no-gutters>
-    <v-col v-if="$route.path !== '/memo/create'" id="welcome-pane" cols="auto" class="pa-0 ma-0 d-flex flex-column">
+    <v-col v-if="!isFocused" id="welcome-pane" cols="auto" class="pa-0 ma-0 d-flex flex-column">
       <MainLayout></MainLayout>
     </v-col>
     <v-col id="interact-pane" class="pa-0 ma-0 d-flex flex-column">
@@ -10,14 +10,17 @@
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted } from 'vue';
 import { getData } from '@/api/apis';
 import MainLayout from '@/components/layout/MainLayout.vue';
-import { onMounted } from 'vue';
 import { useMemoStore } from '@/stores/memoStore';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const memoStore = useMemoStore();
+const isFocused = computed(() => {
+  return route.path.includes('/create') || route.path.includes('/game');
+})
 
 onMounted(async () => {
   if (route.path.includes('/memo') && (!route.path.includes('create') || !route.path.includes('edit'))) {
--- client/src/views/quiz/QuizPage.vue
+++ client/src/views/quiz/QuizPage.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>
-    <h1>퀴즈</h1>
+  <div class="pa-2 container">
+    <v-btn icon="mdi-arrow-left" density="comfortable" color="#FDF8EC" @click="() => router.back()">
+    </v-btn>
   </div>
+  <v-row class="pa-0 ma-0 container" align="center" justify="center">
+    <v-col align="center">
+      <h1>문제입니다.</h1>
+      <v-card max-width="70%">
+        <v-card-title>답을 적으세요.</v-card-title>
+        <v-col align="end">
+          <v-btn rounded class="submit-btn">정답 보기</v-btn>
+        </v-col>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 
+
+const router = useRouter();
 </script>
 
 <style scoped>
-
+.container {
+  background-color: #FDF8EC;
+}
+.submit-btn {
+  background-color: #335447;
+  color: white;
+}
 </style>
