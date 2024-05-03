ade355e6eb13cf3640e92fd4a37d5acab1228152
Youn <dzcoffee@users.noreply.github.com>
2024-05-04 00:33:04 +0900

    오타 등 기타 오류 수정
    fastapi 실행 가능
    pydantic orm_mode와 같은 사소한 오류 수정 필요함

--- domain/quiz/quiz_router.py
+++ domain/quiz/quiz_router.py
@@ -25,7 +25,7 @@ async def Get_memos(skip: int = 0, limit: int = 100, db: Session = Depends(get_d
     return db_memos
 
 #퀴즈 옵션 선택한 거 받아와서 DB에 새로 생성하는 api
-@router.post("", reponse_model=quiz_schema.Quiz_entity)
+@router.post("", response_model=quiz_schema.quiz_eBase)
 async def Create_Quiz_by_Option(quiz_option: quiz_schema.QuizCreate, memo_option: memo_schema.Memo, db: Session = Depends(get_db)):
     quiz_crud.create_quiz(db=db, quiz_option = quiz_option) #퀴즈 옵션 저장
     return quiz_memo_group_crud.create_problem_group(db=db, Quiz=quiz_option, Memo=memo_option) #퀴즈와 메모 연결한 그룹 DB 저장
--- domain/quiz/quiz_schema.py
+++ domain/quiz/quiz_schema.py
@@ -11,12 +11,6 @@ class QuizCreate(quiz_eBase):
     count : int
     difficulty : str
 
-    @field_validator('title', 'count', 'difficulty')
-    def not_empty(cls, v):
-        if not v or not v.strip():
-            raise ValueError('빈 값은 허용되지 않습니다.')
-        return v
-
 class Quiz_entity(quiz_eBase):
 
     class Config:
