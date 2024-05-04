cc4284a8400ace51abda6a48bf1db0c83b2ae198
Urchinode <vrdhan212@gmail.com>
2024-05-05 00:04:49 +0900

    [client] fix: 메모 get api 미동작 현상

--- client/src/views/LayoutPage.vue
+++ client/src/views/LayoutPage.vue
@@ -10,22 +10,13 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
-import { getData } from '@/api/apis';
+import { computed } from 'vue';
 import MainLayout from '@/components/layout/MainLayout.vue';
-import { useMemoStore } from '@/stores/memoStore';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-const memoStore = useMemoStore();
 const isFocused = computed(() => {
   return route.path.includes('/create') || route.path.includes('/game');
-})
-
-onMounted(async () => {
-  if (route.path.includes('/memo') && (!route.path.includes('create') || !route.path.includes('edit'))) {
-    memoStore.memos = await getData('/memo');
-  }
 })
 </script>
 
