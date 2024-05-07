70e681787a98135051bd4c56605a1c9db2083e48
Urchinode <vrdhan212@gmail.com>
2024-05-07 20:48:05 +0900

    [project] fix: url 수정

--- client/src/views/memo/MainPage.vue
+++ client/src/views/memo/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row class="ma-0">
     <v-col class="d-flex flex-column align-center justify-center">
-      <v-img :width="80" :max-height="80" src="@/assets/logo.png" class="ma-0"></v-img>
+      <img width="80" src="@/assets/logo.png" class="ma-0"/>
       <h1>메모를 기록해주세요</h1>
     </v-col>
   </v-row>
