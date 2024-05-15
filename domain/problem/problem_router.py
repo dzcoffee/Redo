f924f56a9a169db57395cd2fe85480001f24d144
Youn <dzcoffee@users.noreply.github.com>
2024-05-15 20:57:59 +0900

    [server] update : 퀴즈 로직을 수정헀습니다.
    1. Problem Table에 'comentary' 라는 Text Column이 추가 되었습니다.
    
    2. 대답을 검증하는 post api는 string의 list를 입력받는 형태입니다.
    만약 객관식이면 "1", "3" 이렇게 선지 번호를 입력받고
    만약 단답식, 주관식이면 "string1", "string2"를 입력받습니다.
    
    3. 객관식은 내부 logic으로 gpt를 거치지 않고 바로 검증됩니다.
    
    4. 단답식, 주관식은 gpt를 통해 검증됩니다.
    
    5. gpt-4o모델을 적용시켰습니다.

--- domain/problem/problem_router.py
+++ domain/problem/problem_router.py
@@ -22,7 +22,7 @@ client = OpenAI(
     api_key = OPENAI_API_KEY
 )
 
-MODEL = "gpt-4-turbo" #json형식 return 받으려면 1106버전 이상 
+MODEL = "gpt-4o" #json형식 return 받으려면 1106버전 이상 
 
 DB_Problem_List = []
 
@@ -46,36 +46,44 @@ async def Create_problems(quiz_id: int, db: Session = Depends(get_db)):
 
     model = MODEL 
 
-    query = f"{db_memo.content}라는 내용을 바탕으로 {db_quiz.type}형태로, {db_quiz.count}개의 문제를 만들어줄래?" #json 형태 반환 시 json이 query에 포함되어 있어야함
+    query = f" '''{db_memo.content}'''라는 내용을 바탕으로 '{db_quiz.type}'형태로, {db_quiz.count}개의 문제를 만들어줄래?" #이것도 토큰 수 절약할 꺼면 영어로 번역하면 됨.
     #difficulty는 일단 제외 테스트 후 추가
 
-    messages = [{"role": "system","content": "You are a helpful quiz maker system and also verify user answers and speak Korean "}, 
+    messages = [{"role": "system","content": "You are a helpful quiz maker system and also speak Korean "}, 
                 {"role": "user","content": query},
-                {"role": "system", "content" : "The output format should be as follows'. The number of quesiton follow in the user input. You must not say 'any other things' before 'Question'. Also you must input Separtor '==========!!' between each Qusetion. If type is '단답식', please answer without options.'\n"
+                {"role": "system", "content" : "The output format should be as follows, but If type is not '객관식', Please answer without option like '@!@!@!@{Option1}', '@!@!@!@{Option2}'.\n The number of quesiton follow the user input, but do not put 'the number' before question, just answer Question string.\n You must not say 'any other things' before 'Question'.\n Also you must input Separtor.\n Answer must be correct Answer.\n Commentary must be explaining how can you find the answer.\n Don't put 'Colon' Before any { } instance. '\n"
                  +"Format:\n"
                  +"{Question}?"
-                 +"==========!!\n"
+                 +"==========!!"
                  + "@!@!@!@{Option1} "
                  + "@!@!@!@{Option2} "
                  + "@!@!@!@{Option3} "
                  + "@!@!@!@{Option4} "
                  +"==========!!"
+                 +"{Answer}"
+                 +"==========!!"
+                 +"{Commentary}"
+                 +"==========!!"
                  + "{Question}?"
-                 +"==========!!\n"
+                 +"==========!!"
                  + "@!@!@!@{Option1} "
                  + "@!@!@!@{Option2} "
                  + "@!@!@!@{Option3} "
                  + "@!@!@!@{Option4} "
-                 + "==========!!\n"
+                 + "==========!!"
+                 +"{Answer}"
+                 +"==========!!"
+                 +"{Commentary}"
+                 +"==========!!"
                  }
                 ]
 
-    response = client.chat.completions.create(model=model, messages=messages) #우선 json형태로 반환받아보기
-    answer = response.choices[0].message.content
+    response = client.chat.completions.create(model=model, messages=messages, temperature=0.8, max_tokens=2048) #temperature 0.8이 한국어에 가장 적합하다는 정보가 있어서 적용시켜봄.
+    answer = response.choices[0].message.content #GPT의 답변 받는 거임.
 
     print(answer)
 
-    divided_problems = answer.split("==========!!") #문제, Option 기준으로 나누기
+    divided_problems = answer.split("==========!!") #문제, Option, Answer, commentary 기준으로 나누기
 
     print(divided_problems)
 
@@ -86,18 +94,31 @@ async def Create_problems(quiz_id: int, db: Session = Depends(get_db)):
         question = divided_problems[problem_counter].strip() + '?'
         problem_counter += 1  # 옵션으로 이동
         
-        # 옵션 처리: 공백으로 구분된 옵션들을 배열로 변환
-        options_str = divided_problems[problem_counter].strip()
-        if options_str:
-            options = [option.strip() for option in options_str.split("@!@!@!@") if option.strip()]
-        else:
+        if db_quiz.type == "객관식":
             options = None
+        else:
+            # 옵션 처리: 공백으로 구분된 옵션들을 배열로 변환
+            options_str = divided_problems[problem_counter].strip()
+            if options_str:
+                options = [option.strip() for option in options_str.split("@!@!@!@") if option.strip()]
+            else:
+                options = None
         problem_counter += 1  # 다음 문제로 이동
 
-        print(options)
+        print(options) 
+
+        Quiz_ans = divided_problems[problem_counter].strip()
+        problem_counter += 1
+
+        print(Quiz_ans)
+
+        Quiz_commentary  = divided_problems[problem_counter].strip()
+        problem_counter += 1
+
+        print(Quiz_commentary)
 
         # 문제 객체 생성 및 리스트에 추가
-        db_problem = problem_crud.create_problem(db, quiz_id=quiz_id, question=question, options=options or [], difficulty=db_quiz.difficulty)
+        db_problem = problem_crud.create_problem(db, quiz_id=quiz_id, question=question, options=options or [], difficulty=db_quiz.difficulty, answer = Quiz_ans, comentary= Quiz_commentary)
         problem_list.append(db_problem)
 
     return problem_list #Problem을 List형식으로 반환
@@ -105,79 +126,125 @@ async def Create_problems(quiz_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/{quiz_id}") #우선 모든 질문에 모두 답하는 것으로 함.
-async def Check_User_Answer(problems: List[problem_schema.problem], quiz_id :int, user_answer: str):
+async def Check_User_Answer(problems: List[problem_schema.problem], quiz_id :int, user_answer: List[str]):
 
     history = ""
+    type = 0 # 0 : 단답, 주관식 , 1: 객관식
 
-    count = 1
-    print(problems)
-    for problem in problems:
-        print(problem)
-        history += f"{problem.question}\n"
-        print(history)
-        if problem.options:
-            print(problem.options)
-            options_str = "\n".join([f"{i+1}: {option}" for i, option in enumerate(problem.options)])
-            print(options_str)
-            history += f"{options_str}\n\n"
-        else:
-            history += "선택지 없음\n\n"
-        count = count +1
+    final_dict = {} #반환하는 dict 형 변수
 
-    print("we are out\n")
-    model = MODEL
-
-    print(history)
-
-    query = f"Please verify '{user_answer}' is correct, as the Answer of Question : \n{history} ."
-
-    messages = [{"role": "system", "content" : "Check the before you maded question and verify user answers.\n If user answer is correct, please say in korean."},
-                {"role": "user", "content" : query},
-                {"role": "assistant", "content" : "Please verify the answer of input question is Correct or False.\n And then Teach me what real answer is and why it is. \n"
-                 +  "If there are Options and user answer is '1, 4', The first question of user answer is '1' and the next question of user answer is '4'.  \n"
-                 + " {gpt_answer} must follow user_answer form, if user answer is number(this is option number), you must answer as number. Else if user answer is string, you must answer as string .\n"
-                 + "{gpt_Ture_False} answer form must be 'True' or 'False'"
-                 + "The output format should be as follows. \n"
-                 + "Format:"
-                 + "{question1} ==========!!"
-                 + "{user_answer1} ==========!! {gpt_answer} ==========!! {gpt_Ture_False}==========!!"
-                 + "{gpt_exaplanation_reason}@@==========@@"
-                 + "{question2}==========!!"
-                 + "{user_answer2}==========!!{gpt_answer}==========!!"
-                 + "{gpt_exaplanation_reason}@@==========@@"
-                 }]
+    if problems[0].options: #첫 문제로 단답, 객관식 판별(option있으면 객관식)
+        type = 1
     
-    response = client.chat.completions.create(model=model, messages=messages)
-    answer = response.choices[0].message.content
-
-    sets = answer.strip().split("@@==========@@")
-    final_dict = {}
-
-    print ("before loop")
-
-    # 각 세트를 순회하며 처리
-    for i, set in enumerate(sets, start=1):
-        if set:  # 세트가 비어있지 않은 경우에만 처리
-            parts = set.strip().split("==========!!")
-            # 세부 항목 분리 및 딕셔너리 구성
-            question = parts[0].strip()
-            user_answer = parts[1].strip()
-            gpt_answer = parts[2].strip()
-            gpt_TF = parts[3].strip()
-            reason = parts[4].strip()
-
-            # 최종 딕셔너리에 추가
-            final_dict[i] = {
-                "question": question,
-                "user_answer": user_answer,
-                "gpt_answer": gpt_answer,
-                "gpt_TF" : gpt_TF,
-                "reason": reason
+    problem_count = 0
+    if(type == 1):
+        print("객관식임\n")
+        for problem in problems:
+            check_answer = user_answer[problem_count] #N번째 문제의 유저 답변 
+            int(check_answer) # str형 -> int형
+            User_TF = "False"
+            print(check_answer)
+            print(problem.options[check_answer])
+            print(problem.answer)
+            if problem.options[check_answer] == problem.answer: #해당 선지 번호의 답변과 problem.answer가 동일하다면 True
+                User_TF = "True"
+            
+            final_dict[problem_count] = {
+                "question": problem.question,
+                "user_answer": check_answer,
+                "gpt_answer": problem.answer,
+                "gpt_TF" : User_TF,
+                "reason": problem.comentary
             }
+            problem_count = problem_count +1
 
-    # 결과 출력
-    print(final_dict)
+             # 결과 출력
+            print(final_dict)
+            
 
-    print("ended\n")
+    if(type == 0):
+        print("주관식임\n")
+        count = 1
+        print(problems)
+        for problem in problems: 
+            history += f"Question {count} : {problem.question}\n"
+
+            ## 원래 객관식 option 처리해주던 거
+            # if problem.options:
+            #     print(problem.options)
+            #     options_str = "\n".join([f"{i+1}: {option}" for i, option in enumerate(problem.options)])
+            #     print(options_str)
+            #     history += f"{options_str}\n\n"
+            # else:
+            #     history += "선택지 없음\n\n"
+            count = count +1
+
+        print("we are out\n")
+        model = MODEL
+
+
+        query = f"Please verify {user_answer} is correct, as the Answer of Question : {history} ."
+
+        messages = [{"role": "system", "content" : "Check the before you maded question and verify user answers.\n"},
+                    {"role": "user", "content" : query},
+                    {"role": "assistant", "content" : "Please verify the answer of input question is Correct or False.\n And then Teach me what real answer is and why it is. \n"
+                    +  "If there are Options and user answer is ['A1', 'A2'], The Question 1 of user answer is 'A1' and the Question 2 of user answer is 'A2'.  \n"
+                    + "{gpt_answer} is Only A Answer that you verifed by question, '''Do not contain a Question.'''  \n"
+                    + "{gpt_Ture_False} form must be 'True' or 'False'.\n"
+                    + "{gpt_exaplanation_reason} is 'the reason' user answer is True or False.\n"
+                    + "And each Set(gpt_answer, gpt_True_False, gpt_answer) must allocated at A Question and Each Set is divided by seperator '@@==========@@'.\n"
+                    + "Please do not answer with any 'Colon', Attribute like 'gpt_answer:' and bracket."
+                    + "The output format must be as follows with korean.\n"
+                    + "Format:"
+                    + "{gpt_answer}==========!!{gpt_Ture_False}==========!!"
+                    + "{gpt_exaplanation_reason}@@==========@@"
+                    + "{gpt_answer}==========!!{gpt_Ture_False}==========!!"
+                    + "{gpt_exaplanation_reason}@@==========@@"
+                    }]
+        
+        
+        response = client.chat.completions.create(model=model, messages=messages)
+        answer = response.choices[0].message.content
+
+        print(answer)
+
+        print("\n\nit is answer \n\n ")
+
+        sets = answer.strip().split("@@==========@@")
+        
+
+        print (sets)
+
+        print("\nthis is user answer : \n")
+        print(user_answer)
+
+        # 각 세트를 순회하며 처리
+        count_num = 0 #아래 problems와 user_answer 체크용 변수
+        for i, set in enumerate(sets, start=1):
+            if set:  # 세트가 비어있지 않은 경우에만 처리
+                parts = set.strip().split("==========!!")
+                # 세부 항목 분리 및 딕셔너리 구성
+                question = problems[count_num].question
+                answer_by_user = user_answer[count_num]
+                gpt_answer = parts[0].strip()
+                gpt_TF = parts[1].strip()
+                reason = parts[2].strip()
+
+                # 최종 딕셔너리에 추가
+                final_dict[i] = {
+                    "question": question,
+                    "user_answer": answer_by_user,
+                    "gpt_answer": gpt_answer,
+                    "gpt_TF" : gpt_TF,
+                    "reason": reason
+                }
+                count_num = count_num+1
+
+        # 결과 출력
+        print(final_dict)
+
+       
+
+        print("ended\n")
 
     return final_dict
