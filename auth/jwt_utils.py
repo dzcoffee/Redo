1bd596f2d47bff56841b16e701844e3121b3c155
Urchinode <vrdhan212@gmail.com>
2024-06-02 15:35:37 +0900

    [server] fix: 토큰 만료기간 한국시간 설정

--- auth/jwt_utils.py
+++ auth/jwt_utils.py
@@ -1,20 +1,21 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from domain.security.token_schema import TokenData
 from datetime import datetime, timedelta
 from utils.logger import logger
+from utils.time import kst
 import jwt
+import time
 
 # OAuth2PasswordBearer를 사용하여 토큰을 추출합니다.
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 SECRET_KEY = "zvojpryvdipvooazbtfkbjcrhnnumpjwlmbhrvyatjbyevzhjwkyyzgivsxhherq"
 ALGORITHM = "HS256"
-EXPIRATION_PERIOD = 30 # 30분
+EXPIRATION_PERIOD = 1440 # 하루
 
 def verify_token(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
-        status_code=401,
+        status_code=403,
         detail="토큰 검증 실패",
         headers={"WWW-Authenticate": "Bearer"},
     )
@@ -22,18 +23,18 @@ def verify_token(token: str = Depends(oauth2_scheme)):
     try:
         # 페이로드 추출
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
+        return payload if payload['exp'] >= datetime.now(kst).timestamp() else None
         
-        # username이 없으면 예외
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
     except jwt.PyJWTError:
         raise credentials_exception
-    
-    # 검증된 토큰 데이터를 반환합니다.
-    return token_data
+
+def refresh_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    if payload and payload['exp'] >= datetime.now(kst).timestamp():
+        return create_token(payload['accountId'])
+    return None
 
 def create_token(user_id):
-    expiration = datetime.now() + timedelta(minutes=EXPIRATION_PERIOD)
+    expiration = datetime.now(kst) + timedelta(minutes=EXPIRATION_PERIOD)
     return jwt.encode({'accountId': user_id, 'exp': expiration, 'iss': 'redo'}, SECRET_KEY, algorithm=ALGORITHM)
---
+++ utils/time.py
@@ -0,0 +1,4 @@
+from datetime import timedelta, timezone
+
+# 현재 한국 표준시 (KST) 시간
+kst = timezone(timedelta(hours=9))
