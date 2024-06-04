8a87cb5c9ad9ebe67fc1b7f42fd6b5f5bf1262ca
vrdhan212 <vrdhan212@gmail.com>
2024-06-04 11:15:21 +0900

    [server] config: jwt 비밀번호 관련 유틸리티 추가

--- auth/auth.py
+++ auth/auth.py
@@ -2,20 +2,18 @@ from fastapi import Request
 from sqlalchemy.orm import Session
 from domain.user.user_schema import AuthRequest
 from models import User
-from passlib.context import CryptContext
 
 from auth.jwt_utils import get_user
 from utils.logger import logger
-import jwt
+from utils.password import password_context
 
 # 사용자 인증 및 토큰 생성
 
 # 사용자 인증 함수
 def authenticate_user(db: Session, request: AuthRequest):
-    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     user = db.query(User).filter(User.accountID == request.accountID).first()
     logger.info(f'Query User: {user}')
-    if not user or not pwd_context.verify(request.password, user.password):
+    if not user or not password_context.verify(request.password, user.password):
         return False
     return user
 
