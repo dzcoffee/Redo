0b32936f8a1e6e2efa4f9d6081e676594e1c42e2
Urchinode <vrdhan212@gmail.com>
2024-06-12 22:59:49 +0900

    [client] fix: mdi 아이콘 미표시

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -6,6 +6,7 @@ import 'vuetify/styles'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 /**
  * @description
@@ -13,7 +14,14 @@ import { createVuetify } from 'vuetify'
  */
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
 })
 
 export default vuetify
